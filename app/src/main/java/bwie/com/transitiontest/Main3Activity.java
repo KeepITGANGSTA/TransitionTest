f7d2ba65ff7a58c9f5f056b6533fdaac614be4a7
KeepITGANGSTA <31384766+KeepITGANGSTA@users.noreply.github.com>
2017-11-28 17:40:11 +0800

    Initial commit

---
+++ app/src/main/java/bwie/com/transitiontest/Main3Activity.java
@@ -0,0 +1,24 @@
+package bwie.com.transitiontest;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void btnOnclick(View view) {
+        Intent intent=new Intent(this,Main2Activity.class);
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+    }
+}
---
+++ app/src/main/java/utils/Test.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ * Created by 李英杰 on 2017/11/28.
+ * Description：
+ */
+
+public class Test {
+
+    public void say(){
+
+    }
+
+}
